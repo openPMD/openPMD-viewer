23af8e56af8c2ff6a01917d1a5c884f97209333c
Remi Lehe <rlehe@lbl.gov>
2015-10-08 10:51:21 -0700

    Corrected pyflakes warnings

--- opmd_viewer/__init__.py
+++ opmd_viewer/__init__.py
@@ -1,1 +1,3 @@
+# Make the OpenPMDTimeSeries object accessible from outside the package
 from .openpmd_timeseries import OpenPMDTimeSeries
+__all__ = ['OpenPMDTimeSeries']
--- opmd_viewer/openpmd_timeseries/__init__.py
+++ opmd_viewer/openpmd_timeseries/__init__.py
@@ -1,1 +1,3 @@
+# Make the OpenPMDTimeSeries accessible from outside the file main
 from .main import OpenPMDTimeSeries
+__all__ = ['OpenPMDTimeSeries']
