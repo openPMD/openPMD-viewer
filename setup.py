3c2a329f6c2c2a17a7f112cba0932c1efef8e8e6
Remi Lehe <remi.lehe@normalesup.org>
2016-04-28 21:52:37 -0700

    Updated setup.py in preparation for PyPi
    Followed instructions at:
    http://python-packaging-user-guide.readthedocs.io/en/latest/distributing/

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
-#!/usr/bin/env python
 from setuptools import setup, find_packages
 import opmd_viewer  # In order to extract the version number
 
+# Get the long description
+with open('./README.md') as f:
+    long_description = f.read()
 # Get the package requirements from the requirements.txt file
-with open('requirements.txt') as f:
+with open('./requirements.txt') as f:
     install_requires = [line.strip('\n') for line in f.readlines()]
 # Since wget cannot be installed with conda, it is added separately here
 install_requires.append('wget')
@@ -11,31 +13,29 @@ install_requires.append('wget')
 # Main setup command
 setup(name='openPMD-viewer',
       version=opmd_viewer.__version__,
+      description='Visualization tools for openPMD files',
+      long_description=long_description,
       maintainer='Remi Lehe',
       maintainer_email='remi.lehe@lbl.gov',
-      description='Visualization tools for openPMD files',
-      url='git@bitbucket.org:berkeleylab/opmd_viewer.git',
+      license='BSD 3-Clause License',
+      url='https://github.com/openPMD/openPMD-viewer.git',
       packages=find_packages('./'),
       package_data={'opmd_viewer': ['notebook_starter/*.ipynb']},
       scripts=['opmd_viewer/notebook_starter/openPMD_notebook'],
       install_requires=install_requires,
       tests_require=['pytest', 'jupyter'],
       setup_requires=['pytest-runner'],
-      platforms='any',
       classifiers=[
           'Programming Language :: Python',
           'Development Status :: 4 - Beta',
           'Natural Language :: English',
           'Environment :: Console',
-          'Environment :: Jupyter',
           'Intended Audience :: Science/Research',
-          'License :: BSD-3-Clause-LBNL',
+          'License :: OSI Approved :: BSD License',
           'Operating System :: OS Independent',
           'Topic :: Scientific/Engineering :: Physics',
           'Topic :: Scientific/Engineering :: Visualization',
-          'Topic :: Database :: Front-Ends',
           'Programming Language :: Python :: 2.7',
           'Programming Language :: Python :: 3.4',
-          'Programming Language :: Python :: 3.5',
-      ]
+          'Programming Language :: Python :: 3.5'],
       )
